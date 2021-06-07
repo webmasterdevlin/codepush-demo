bb869ed7981026f345181870f70d1787c9020ed8
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-06-07 17:45:57 +0200

    updated

--- android/app/src/main/java/com/codepushdemo/generated/BasePackageList.java
+++ android/app/src/main/java/com/codepushdemo/generated/BasePackageList.java
@@ -13,8 +13,15 @@ public class BasePackageList {
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
-        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.imagemanipulator.ImageManipulatorPackage(),
         new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.medialibrary.MediaLibraryPackage(),
+        new expo.modules.notifications.NotificationsPackage(),
+        new expo.modules.screenorientation.ScreenOrientationPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage()
     );

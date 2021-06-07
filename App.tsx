95212d57b333b2aa9b57cbc8efc9cf7bf682313e
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-06-07 14:53:24 +0200

    updated

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import codePush from "react-native-code-push";
+
+const App = codePush(() => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
-}
+});
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
